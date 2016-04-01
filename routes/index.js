a19d487f0643d3057e20a773a1b61dc66db609cb
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-01 10:15:05 -0600

    added noun input. successfully inserts to noun db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('../knexfile')['development']);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/fablibs', function(req, res, next) {
+  res.render('fablibs');
+});
+
+router.post('/addnoun', function(req, res, next) {
+  knex('nouns')
+  .insert(req.body)
+  .then(function ( response ){
+    res.redirect('/fablibs');
+  })
+});
+
+
 
 module.exports = router;
