7819a44ef0cc74af85dc60a33fb117fe749dcfdc
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-01 09:31:18 -0600

    added users, nouns, verbs, and adjectives tables

--- migrations/20160401092551_create_tables.js
+++ migrations/20160401092551_create_tables.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function( table ){
+    table.increments();
+    table.string('userName');
+    table.string('password');
+  });
+  return knex.schema.createTable('nouns', function( table ){
+    table.increments();
+    table.string('word');
+  });
+  return knex.schema.createTable('verbs', function( table ){
+    table.increments();
+    table.string('word');
+  });
+  return knex.schema.createTable('adjectives', function( table ){
+    table.increments();
+    table.string('word');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
+  return knex.schema.dropTable('nouns');
+  return knex.schema.dropTable('verbs');
+  return knex.schema.dropTable('adjectives');
 };
