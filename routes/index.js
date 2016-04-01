6400d88ff358c1a03a417de358fd1f0d0c26a129
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-01 11:00:59 -0600

    installed fs. added .txt file. create writeFile / readFile feature for fablibs input. data of .txt file is appended to /fablibs

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')['development']);
 var bcrypt = require('bcryptjs');
+var fs = require('fs');
 
 
 /* GET home page. */
@@ -20,32 +21,35 @@ router.get('/home', function(req, res, next) {
 router.post('/user/register', function(req, res, next) {
   var hash = bcrypt.hashSync(req.body.password, 8);
   knex('users')
-    .insert({
-      'userName': req.body.username.toLowerCase(),
-      'password': hash
-    })
-    .then(function(response) {
-      res.redirect('/');
-    })
+  .insert({
+    'userName': req.body.username.toLowerCase(),
+    'password': hash
+  })
+  .then(function(response) {
+    res.redirect('/');
+  })
 });
 
 router.post('/user/login', function(req, res, next) {
   knex('users')
-    .where('userName', '=', req.body.username.toLowerCase())
-    .first()
-    .then(function(response) {
-      if (response && bcrypt.compareSync(req.body.password, response.password)) {
-        res.redirect('/home');
-      } else {
-        res.render('auth', {
-          error: 'Invalid username or password'
-        });
-      }
-    });
+  .where('userName', '=', req.body.username.toLowerCase())
+  .first()
+  .then(function(response) {
+    if (response && bcrypt.compareSync(req.body.password, response.password)) {
+      res.redirect('/home');
+    } else {
+      res.render('auth', {
+        error: 'Invalid username or password'
+      });
+    }
+  });
 });
 
 router.get('/fablibs', function(req, res, next) {
-  res.render('fablibs');
+  fs.readFile('fablibs.txt','utf8', function( err, data ){
+    if ( err ) res.send('error:' + err);
+    res.render('fablibs', {output: data});
+  })
 });
 
 router.post('/addnoun', function(req, res, next) {
@@ -70,6 +74,13 @@ router.post('/addadj', function(req, res, next) {
   })
 });
 
+router.post('/translate', function(req, res, next) {
+  console.log(req.body.fablibs);
+  fs.writeFile('fablibs.txt', req.body.fablibs, 'utf8', function( err ){
+    if ( err ) res.send('error:' + err);
+  });
+  res.redirect('/fablibs')
+});
 
 
 module.exports = router;
